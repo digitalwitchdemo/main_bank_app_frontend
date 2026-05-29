a8f0561bd6e999d500eb2b9bf515ffe600426d65
Ella <imohellanneamaka@gmail.com>
2026-05-29 14:01:52 +0100

    first commit

--- src/BankTransactionComponent/ViewCustomerTransactions.jsx
+++ src/BankTransactionComponent/ViewCustomerTransactions.jsx
@@ -29,7 +29,7 @@ const ViewCustomerTransactions = () => {
 
   const retrieveAllTransactions = async () => {
     const response = await axios.get(
-      "https://bankapi.cloudwitches.online/api/bank/transaction/history?userId=" +
+      "https://bankapi.pod15engineers.online/api/bank/transaction/history?userId=" +
         customer.id,
       {
         headers: {
